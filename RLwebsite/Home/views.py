9db2ebdf896ca4a3c426e725030c76f657756d53
Vinit Choudhary <vinitchahal1241@gmail.com>
2025-03-05 10:15:44 +0530

    Removing of unwanted code comments

--- RLwebsite/Home/views.py
+++ RLwebsite/Home/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-
 # Create your views here.
 def homepage(request):
     return render(request,'index.html')
@@ -15,7 +14,7 @@ def product(request):
     return render(request, 'Products.html')
 
 def service(request):
-    return render(request, 'services.html')
+    return render(request, 'Services.html')
 
 def outcome(request):
     return render(request, 'Outcomes.html')
@@ -23,19 +22,15 @@ def outcome(request):
 def innovation(request):
     return render(request, 'innovation.html')
 
-
-
-
-
 def userlogin(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
 
         if username == 'karry' and password == '2310':
-            # Initialize or clear session data
             request.session['responses'] = {}
-            return redirect('raslo')  # Redirect to raslo view
+            return redirect('raslo')  
+        
         else:
             context={'message':"The username or password you entered is incorrect."}
             return render(request, 'login.html',context)
